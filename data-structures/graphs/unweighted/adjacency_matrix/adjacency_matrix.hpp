3628a2be73dddf082d25e02138ecca6ecfe17420
armanmikoyan <>
2024-01-20 17:16:25 +0400

    Koasaraju algorithm matrix

--- data-structures/graphs/unweighted/adjacency_matrix/adjacency_matrix.hpp
+++ data-structures/graphs/unweighted/adjacency_matrix/adjacency_matrix.hpp
@@ -37,14 +37,26 @@ public:
     std::vector<int>                                               topological_sort() const;
     std::vector<int>                               topological_sort_Kahns_algorithm() const;
 
+    //SCC
+    std::vector<std::vector<int>>                           scc_Kosarajus_algorithm() const;
+
 private: // helpers
     void dfs_helper_recrusive(size_t, std::vector<bool>&, std::function<void(size_t)>) const;
     void dfs_helper_iterative(size_t, std::vector<bool>&, std::function<void(size_t)>) const;
     void          bfs_helper(size_t, std::vector<bool>&,  std::function<void(size_t)>) const;
-    bool                has_cycle_undirected_helper(size_t, size_t, std::vector<bool>&) const;
-    bool      has_cycle_directed_helper(size_t, std::vector<bool>&, std::vector<bool>&) const;
-    std::vector<int>                     reconstruct(size_t, size_t, std::vector<int>&) const;
-    void         topological_sort_helper(size_t, std::vector<bool>&, std::vector<int>&) const;
+    bool               has_cycle_undirected_helper(size_t, size_t, std::vector<bool>&) const;
+    bool     has_cycle_directed_helper(size_t, std::vector<bool>&, std::vector<bool>&) const;
+    std::vector<int>                    reconstruct(size_t, size_t, std::vector<int>&) const;
+    void        topological_sort_helper(size_t, std::vector<bool>&, std::vector<int>&) const;
+
+    void scc_Kosarajus_algorithm_helper_first_pass(size_t, 
+                                                   std::vector<bool>&, 
+                                                   std::stack<size_t>&) const;
+
+    void scc_Kosarajus_algorithm_helper_second_pass(size_t,
+                                                    std::vector<bool>&,
+                                                    std::vector<int>&,
+                                                    std::vector<std::vector<int>>&) const;
 
     void all_paths_two_vertex_helper(size_t,
                                      size_t,
--- data-structures/graphs/unweighted/adjacency_matrix/main.cpp
+++ data-structures/graphs/unweighted/adjacency_matrix/main.cpp
@@ -2,49 +2,35 @@
 
 int main()
 {
-    graph_matrix<int> g{};
+   graph_matrix<int> g{};
     g.add_vertex();
     g.add_vertex();
     g.add_vertex();
     g.add_vertex();
     g.add_vertex();
     g.add_vertex();
-    g.add_vertex();
-    g.add_vertex();
-    g.add_vertex();
-    g.add_vertex();
-    g.add_vertex();
-    g.add_vertex();
-    g.add_vertex();
-
-    g.add_edge(1, 0);
-    g.add_edge(1, 3);
-    g.add_edge(0, 2);
-    g.add_edge(3, 2);
+    g.add_edge(0, 1);
+    g.add_edge(1, 5);
+    g.add_edge(5, 4);
     g.add_edge(2, 4);
-    g.add_edge(2, 7);
-    g.add_edge(4, 5);
-    g.add_edge(7, 5);
-    g.add_edge(7, 8);
-    g.add_edge(5, 6);
-    g.add_edge(8, 9);
-    g.add_edge(10, 8);
-    g.add_edge(11, 8);
-    g.add_edge(11, 10);
-    g.add_edge(12, 0);
-    g.add_edge(12, 11);
-    g.add_edge(12, 2);
+    g.add_edge(1, 2);
+    g.add_edge(2, 3);
+    g.add_edge(3, 0);
 
     g.print_matrix();
 
-    auto list = g.topological_sort_Kahns_algorithm();
+    auto list = g.scc_Kosarajus_algorithm();
 
     for (auto vertice : list)
     {
-        std::cout << vertice << " ";
+        for (auto v : vertice)
+        {
+            std::cout << v << " ";
+        }
+        std::cout << std::endl;
     }
-
-    std::cout << std::endl;
+  
+  
 }
 
 
@@ -154,6 +140,78 @@ graph_list<int> g{};
 
 
 
+
+
+// Test cases for Kosaraju's  algorithm
+
+
+Test 1
+
+graph_matrix<int> g{};
+    g.add_vertex();
+    g.add_vertex();
+    g.add_vertex();
+    g.add_vertex();
+    g.add_vertex();
+    g.add_vertex();
+    g.add_edge(0, 1);
+    g.add_edge(1, 5);
+    g.add_edge(5, 4);
+    g.add_edge(2, 4);
+    g.add_edge(1, 2);
+    g.add_edge(2, 3);
+    g.add_edge(3, 0);
+
+    g.print_matrix();
+
+    auto list = g.scc_Kosarajus_algorithm();
+
+    for (auto vertice : list)
+    {
+        for (auto v : vertice)
+        {
+            std::cout << v << " ";
+        }
+        std::cout << std::endl;
+    }
+
+Test 2
+
+    graph_matrix<int> g{};
+    g.add_vertex();
+    g.add_vertex();
+    g.add_vertex();
+    g.add_vertex();
+    g.add_vertex();
+    g.add_vertex();
+    g.add_vertex();
+    g.add_vertex();
+    g.add_vertex();
+    g.add_edge(0, 1);
+    g.add_edge(1, 0);
+    g.add_edge(0, 2);
+    g.add_edge(2, 0);
+    g.add_edge(2, 3);
+    g.add_edge(3, 4);
+    g.add_edge(4, 7);
+    g.add_edge(4, 5);
+    g.add_edge(5, 6);
+    g.add_edge(6, 4);
+    g.add_edge(4, 7);
+    g.add_edge(7, 8);
+
+    g.print_matrix();
+
+    auto list = g.scc_Kosarajus_algorithm();
+
+    for (auto vertice : list)
+    {
+        for (auto v : vertice)
+        {
+            std::cout << v << " ";
+        }
+        std::cout << std::endl;
+    }
 
 
 #endif
