360d9e2eddd1875692af0a12409f946c60a315e1
Some test <test@test.com>
2024-01-23 17:54:57 +0400

    Tests

--- data-structures/graphs/unweighted/adjacency_list/main.cpp
+++ data-structures/graphs/unweighted/adjacency_list/main.cpp
@@ -2,49 +2,38 @@
 
 int main()
 {
-   graph_list<int> g{};
-    g.add_vertex();
-    g.add_vertex();
-    g.add_vertex();
-    g.add_vertex();
-    g.add_vertex();
-    g.add_vertex();
-    g.add_vertex();
-    g.add_vertex();
-    g.add_vertex();
-    g.add_vertex();
-    g.add_vertex();
-    g.add_vertex();
-    g.add_vertex();
 
-    g.add_edge(1, 0);
-    g.add_edge(1, 3);
-    g.add_edge(0, 2);
-    g.add_edge(3, 2);
-    g.add_edge(2, 4);
-    g.add_edge(2, 7);
+graph_list<int> g{};
+
+    g.add_vertex();
+    g.add_vertex();
+    g.add_vertex();
+    g.add_vertex();
+    g.add_vertex();
+    g.add_vertex();
+    g.add_vertex();
+    g.add_edge(0, 1);
+    g.add_edge(1, 2);
+    g.add_edge(2, 1);
     g.add_edge(4, 5);
-    g.add_edge(7, 5);
-    g.add_edge(7, 8);
-    g.add_edge(5, 6);
-    g.add_edge(8, 9);
-    g.add_edge(10, 8);
-    g.add_edge(11, 8);
-    g.add_edge(11, 10);
-    g.add_edge(12, 0);
-    g.add_edge(12, 11);
-    g.add_edge(12, 2);
+    g.add_edge(5, 4);
+    g.add_edge(6, 5);
+    g.add_edge(6, 3);
+    g.add_edge(3, 2);
+    g.add_edge(2, 0);
 
-    g.print_list();
-
-    auto list = g.topological_sort_Kahns_algorithm();
+    auto list = g.scc_Tarjans_algorithm();
 
     for (auto vertice : list)
     {
-        std::cout << vertice << " ";
+        for (auto v : vertice)
+        {
+            std::cout << v << " ";
+        }
+        std::cout << std::endl;
     }
 
-  
+
 }
 
 
@@ -295,6 +284,6 @@ TEST 2
     g.add_edge(1, 2);
     g.add_edge(2, 3);
     g.add_edge(3, 0);
-    
+
 
 #endif
