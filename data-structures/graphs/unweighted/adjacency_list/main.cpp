3ba080c5f7b12b2d85b2a1afe41b6d8df1e08257
armanmikoyan <>
2024-01-23 16:44:55 +0400

    ch

--- data-structures/graphs/unweighted/adjacency_list/main.cpp
+++ data-structures/graphs/unweighted/adjacency_list/main.cpp
@@ -2,8 +2,7 @@
 
 int main()
 {
-    graph_list<int> g{};
-     g.add_vertex();
+   graph_list<int> g{};
     g.add_vertex();
     g.add_vertex();
     g.add_vertex();
@@ -12,28 +11,37 @@ int main()
     g.add_vertex();
     g.add_vertex();
     g.add_vertex();
-    g.add_edge(0, 1);
+    g.add_vertex();
+    g.add_vertex();
+    g.add_vertex();
+    g.add_vertex();
+    g.add_vertex();
+
     g.add_edge(1, 0);
+    g.add_edge(1, 3);
     g.add_edge(0, 2);
-    g.add_edge(2, 0);
-    g.add_edge(2, 3);
-    g.add_edge(3, 4);
-    g.add_edge(4, 7);
+    g.add_edge(3, 2);
+    g.add_edge(2, 4);
+    g.add_edge(2, 7);
     g.add_edge(4, 5);
-    g.add_edge(5, 6);
-    g.add_edge(6, 4);
-    g.add_edge(4, 7);
+    g.add_edge(7, 5);
     g.add_edge(7, 8);
+    g.add_edge(5, 6);
+    g.add_edge(8, 9);
+    g.add_edge(10, 8);
+    g.add_edge(11, 8);
+    g.add_edge(11, 10);
+    g.add_edge(12, 0);
+    g.add_edge(12, 11);
+    g.add_edge(12, 2);
+
     g.print_list();
-    auto list = g.scc_Tarjans_algorithm();
+
+    auto list = g.topological_sort_Kahns_algorithm();
 
     for (auto vertice : list)
     {
-        for (auto v : vertice)
-        {
-            std::cout << v << " ";
-        }
-        std::cout << std::endl;
+        std::cout << vertice << " ";
     }
 
   
